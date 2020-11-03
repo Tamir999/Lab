10af6918c1474747f4ee78420bb314e47acc6e9b
Tamir999 <73513763+Tamir999@users.noreply.github.com>
2020-11-03 12:35:05 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { HeaderTop } from './HeaderTop.js';
-import { HeaderLarge } from './HeaderLarge.js';
-
-export function App(props) {
-    return React.createElement(
-        'div',
-        null,
-        React.createElement(HeaderTop, null),
-        React.createElement(HeaderLarge, null)
-    );
+import { BrowserRouter, Link, Switch, Route, useParams } from "react-router-dom";
+import React from 'react';
+import {HeaderTop} from './HeaderTop.js'
+import {HeaderLarge} from './HeaderLarge.js'
+export function App() {
+  let { id } = useParams();
+  return (
+    <div>
+       {/* <HeaderTop/> */}
+            <HeaderLarge/>
+            <h1>{id}</h1>
+    </div>
+  );
 }
--- HeaderTop.js
+++ HeaderTop.js
@@ -1,3 +1,4 @@
+import React from 'react';
 export function HeaderTop() {
     return React.createElement(
         "div",
